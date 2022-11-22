7682ca0e354a027797a1173e2be8b8b7f70e9bc6
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-11-22 08:25:22 -0700

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            
+            Console.WriteLine("How many siblings do you have?");
+            string siblings = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite food?");
+            string food = Console.ReadLine();
+
+            Console.Write("Hello world! I am the great an powerfull " + name);
+            Console.Write("! I love wearing fancy " + color);
+            Console.Write(" capes. In my spare time I like to juggle " + food);
+            Console.Write(". I can get " + siblings);
+            Console.Write(" items in the air at once!");
+           
         }
     }
 }
