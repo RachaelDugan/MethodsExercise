ceab5df4dc29a9f55224fefaf963c9a15a1fcb8c
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-11-22 10:37:29 -0700

    both exercises

---
+++ MethodsExercise/MoreMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class MoreMethods
+
+    {
+         public static void Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+         public static void Subtract(int num1, int num2)
+        {
+            int sum = num1 - num2;
+            Console.WriteLine(sum);
+        }
+        public static void Multiply(int num1, int num2)
+        {
+            int sum = num1 * num2;
+            Console.WriteLine(sum);
+        }
+        public static void Divide(int num1, int num2)
+        {
+            int sum = num1 / num2;
+            Console.WriteLine(sum);
+        }
+       
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MethodsExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MethodsExercise
 {
     public class Program
     {
@@ -22,8 +24,18 @@
             Console.Write("! I love wearing fancy " + color);
             Console.Write(" capes. In my spare time I like to juggle " + food);
             Console.Write(". I can get " + siblings);
-            Console.Write(" items in the air at once!");
-           
+            Console.WriteLine(" items in the air at once!");
+            Console.WriteLine("My lucky numbers are -");
+
+
+
+            //second exercise
+            MoreMethods.Add(4, 9);
+            MoreMethods.Subtract(71, 36);
+            MoreMethods.Multiply(5, 9);
+            MoreMethods.Divide(25, 5);
+
         }
     }
 }
+
